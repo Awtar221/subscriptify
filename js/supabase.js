283058c8936eda166e7c57e92b28994815876a95
Awtar221 <kelvinsingh145@proton.me>
2026-07-13 13:09:56 +0800

    more security issues fixed

--- js/supabase.js
+++ js/supabase.js
@@ -1,5 +1,4 @@
-const supabaseUrl = 'https://stdqfevaqvqajtsrxina.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN0ZHFmZXZhcXZxYWp0c3J4aW5hIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM4NzAzMjksImV4cCI6MjA5OTQ0NjMyOX0.rjBQZQn-zozpY75THyij2eb_5AH88dcN77iDU1DLwLo'
+const { url: supabaseUrl, anonKey: supabaseAnonKey } = window.SUPABASE_CONFIG
 
 // Use window.supabase from CDN
 const supabase = window.supabase.createClient(supabaseUrl, supabaseAnonKey)
